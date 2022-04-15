037db759cf659f8b2b9d1f56be37e98db3f31e7a
suzhouchaosuan <yyxx20150909@163.com>
2022-04-15 09:19:45 +0800

    modify the folder

--- ALBEF/flk_finetune.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-python -m torch.distributed.launch --nproc_per_node=4 --use_env Retrieval.py \
-    --config ./configs/Retrieval_flickr.yaml \
-    --output_dir output/Retrieval_flickr_addte \
-    --checkpoint /data1/yx/suda/image-text/sotas/ALBEF/datas/pre.pth
