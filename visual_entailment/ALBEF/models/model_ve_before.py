35051fc59184f26958e6a3ab64863956b0814bf0
achunhui-666 <1685715822@qq.com>
2022-05-26 23:33:20 +0800

    两个列表的ve

--- visual_entailment/ALBEF/models/model_ve_before.py
+++ visual_entailment/ALBEF/models/model_ve_before.py
@@ -23,8 +23,9 @@ class ALBEF(nn.Module):
             mlp_ratio=4, qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6))
 
         bert_config = BertConfig.from_json_file(config['bert_config'])
-        self.te_bert = tBert.from_pretrained(text_encoder, add_pooling_layer=False)
-        self.text_encoder = BertModel.from_pretrained(text_encoder, config=bert_config, add_pooling_layer=False)
+
+        self.text_encoder = BertModel.from_pretrained(
+            text_encoder, config=bert_config, add_pooling_layer=False)
 
         self.cls_head = nn.Sequential(
             nn.Linear(self.text_encoder.config.hidden_size,
@@ -54,7 +55,8 @@ class ALBEF(nn.Module):
             self.momentum = 0.995
 
         # dependent textual entailment model & joint VE+TE model
-
+        self.te_bert = tBert.from_pretrained(
+            text_encoder, add_pooling_layer=False)
 
         self.textual_cls_head = nn.Sequential(
             nn.Linear(self.text_encoder.config.hidden_size,
@@ -73,8 +75,7 @@ class ALBEF(nn.Module):
         self.gate_net = nn.Linear(2*self.text_encoder.config.hidden_size, 2)
 
     def forward(self, image, text, hypo, targets, alpha=0, train=True,image_bool=None,text_bool=None):
-        prediction_joint = None
-        if image_bool is not None:
+        if image is not None:
             image_embeds = self.visual_encoder(image)
             image_atts = torch.ones(
                 image_embeds.size()[:-1], dtype=torch.long).to(image.device)
@@ -85,28 +86,27 @@ class ALBEF(nn.Module):
                                        return_dict=True
                                        )
             ve_hiddens = output.last_hidden_state[:, 0, :]
-            prediction_ve = self.cls_head(ve_hiddens[image_bool[0]==1])
-        if text_bool is not None:
+            prediction_ve = self.cls_head(ve_hiddens)
+        if text is not None:
             textual_entailment = self.te_bert(text.input_ids,
                                                 attention_mask=text.attention_mask,
                                                 return_dict=True
                                                 )
             te_hiddens = textual_entailment.last_hidden_state[:, 0, :]
-            prediction_te = self.textual_cls_head(te_hiddens[text_bool[0]==1])
-        if image_bool is not None and text_bool is not None:
-            joint_bool = image_bool[0] * text_bool[0]
-            concated_outputs = torch.cat((ve_hiddens[joint_bool==1], te_hiddens[joint_bool==1]), dim=-1)
+            prediction_te = self.textual_cls_head(te_hiddens)
+        if image is not None and text is not None:
+            concated_outputs = torch.cat((ve_hiddens, te_hiddens), dim=-1)
             gated_values = self.gate_net(concated_outputs)
             gated_values = nn.functional.softmax(gated_values, dim=-1)
             # B * 2
             g0 = gated_values[:, 0].unsqueeze(-1)
             g1 = gated_values[:, 1].unsqueeze(-1)
-            joint_hiddens = g0 * ve_hiddens[joint_bool==1] + g1 * te_hiddens[joint_bool==1]
+            joint_hiddens = g0 * ve_hiddens + g1 * te_hiddens
             prediction_joint = self.joint_cls_head(joint_hiddens)
 
         if train:
 
-            loss_te = F.cross_entropy(prediction_te, targets[text_bool[0]==1]) if text_bool[0].sum()!=0 else torch.tensor(float(0),requires_grad=True)
+            loss_te = F.cross_entropy(prediction_te, targets) if text is not None else torch.tensor(0,requires_grad=False)
 
             if self.distill:
                 with torch.no_grad():
@@ -124,12 +124,11 @@ class ALBEF(nn.Module):
                 loss_ve = (1-alpha)*F.cross_entropy(prediction_ve, targets) - alpha*torch.sum(
                     F.log_softmax(prediction_ve, dim=1)*F.softmax(prediction_m, dim=1), dim=1).mean()
             else:
-                loss_ve = F.cross_entropy(prediction_ve, targets[image_bool[0]==1]) if image_bool[0].sum()!=0 else torch.tensor(float(0),requires_grad=True)
+                loss_ve = F.cross_entropy(prediction_ve, targets) if image is not None else torch.tensor(0,requires_grad=False)
 
             
-            loss_joint = F.cross_entropy(prediction_joint, targets[joint_bool==1]) if joint_bool.sum()!=0 else torch.tensor(float(0),requires_grad=True)
-            if loss_joint==0 or loss_te==0 or loss_ve==0:
-                print("true")
+            loss_joint = F.cross_entropy(prediction_joint, targets) if image is not None and text is not None else torch.tensor(0,requires_grad=False)
+
             return loss_ve, loss_te, loss_joint
 
         else:
