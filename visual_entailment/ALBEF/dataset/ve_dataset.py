ec577fc8b93a8eae15ac272fd894ed43933d2648
achunhui-666 <1685715822@qq.com>
2022-07-31 10:44:52 +0800

    0731

--- visual_entailment/ALBEF/dataset/ve_dataset.py
+++ visual_entailment/ALBEF/dataset/ve_dataset.py
@@ -23,6 +23,7 @@ class ve_dataset(Dataset):
         Input agnostic model
         '''
         ann = self.ann[index]
+        image_bool = []
         if "image" in ann.keys():
             if "COCO" in ann["image"]:
                 image_path = "/data1/ach/coco2014/{}".format(ann["image"])
@@ -33,14 +34,20 @@ class ve_dataset(Dataset):
                 image_path = os.path.join(self.image_root,'%s.jpg'%ann['image'])      
             image = Image.open(image_path).convert('RGB')   
             image = self.transform(image)
+            image_bool.append(1)
         else:
             image = Image.open('/data1/ach/image-text/visual_entailment/ALBEF/ve_caption_new/test.jpg').convert('RGB')   
             image = self.transform(image)
+            image_bool.append(0)
+        caption_bool = []
         if "caption" in ann.keys():
+            # if len(ann["caption"]) ==
             captions = '[SEP]'.join([pre_caption(caption, self.max_words) for caption in ann['caption']])
+            caption_bool.append(1)
         else:
             captions = ""
+            caption_bool.append(0)
         sentence = pre_caption(ann['sentence'], self.max_words)
 
-        return image, captions, sentence, self.labels[ann['label']]
+        return image, captions, sentence, self.labels[ann['label']],image_bool,caption_bool
     
